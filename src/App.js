6e7ee5b0bdcf283c81bb5de0dfd39c24983eedfc
AhmedSama <nwnwlala@yahoo.com>
2022-05-04 17:11:40 +0300

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import {BrowserRouter,Route,Switch} from "react-router-dom"
+import Chat from "./pages/chat";
+import Login from "./pages/login";
+
 
 function App() {
+  const [username,setUsername] = useState("")
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 className="nav">CHAT APPLICATION</h1>
+      <BrowserRouter>
+        <Switch>
+          <Route path={"/"} exact>
+            <Login setUser = {setUsername}/>
+          </Route>
+
+          <Route path={"/chat"} exact>
+            <Chat username = {username}/>
+          </Route>
+        </Switch>
+      </BrowserRouter>  
     </div>
   );
 }
